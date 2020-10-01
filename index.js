7af2d06f8639f9705efd7353584a028becd71e94
KimSilly <69381718+KimSilly@users.noreply.github.com>
2020-10-01 11:51:39 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { token } = require('./config.json');
+
+const { readdirSync } = require('fs');
+const { join } = require('path');
+
+client.commands = new Discord.Collection();
+
+const prefix = '//' //자신의 프리픽스
+
+
+const commandFile = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith("js"));
+
+for (const file of commandFile) {
+  const command = require(join(__dirname, "commands", `${file}`));
+  client.commands.set(command.name, command);
+}
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+  console.log(`${client.user.id}로 로그인 성공!`);
+  client.user.setActivity('//help by KIMSILLY') //상태메시지
+});
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+
+  if(message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+    const command = args.shift().toLowerCase();
+
+    if(!client.commands.has(command)) return;
+
+    try {
+      client.commands.get(command).run(client, message, args);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})
+
+client.login(token);
